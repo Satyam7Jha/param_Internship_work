cd2d7e5931e74680f9ba6d07ebe42b3009c7cc26
SatyamJha <7satyamjha@gmail.com>
2022-07-30 23:35:17 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# param_Internship_work
+---
+title: Project
+emoji: 🌖
+colorFrom: pink
+colorTo: gray
+sdk: streamlit
+sdk_version: 1.10.0
+app_file: app.py
+pinned: false
+---
+
+Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
---
+++ app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+
+from pull_news import get_news
+
+header = st.container()
+input_ = st.container()
+body = st.container()
+
+
+st.markdown(
+    """
+    <style>
+    .main{
+    background-color: #F5F5F5;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+with header:
+    st.title("Welcom to my Project")
+    st.text("In this Project I use web scraping to fetch the news.")
+
+    # taking input
+
+    # topic = st.selectbox("Select Topic: ", options=["India", "Business"])
+    topic = st.text_input("Search for topics,locations & sources", "India")
+
+news_data = get_news(topic)
+
+
+with body:
+    for news in news_data:
+        try:
+
+            st.markdown(f"## {news['publisher']}")
+            image, text = st.columns(2)
+            image.markdown(f"![Image not available!!]({news['imgUrl']})")
+            text.markdown(f"#### {news['title']}.")
+            text.text(f"{news['time']}.")
+            text.markdown(f"[View Full article]({news['url']})")
+            st.markdown("--------------------------------------------------------")
+        except:
+            pass
